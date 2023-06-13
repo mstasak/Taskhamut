5e72319699fe81a7c224add8ec3f7deb6d41dcc4
Mark Stasak <mstasak@cfl.rr.com>
2023-06-13 06:03:21 -0400

    Move task list into main area and replace nn task count placeholder with real value.

--- Taskhamut/ViewModels/TasksViewModel.cs
+++ Taskhamut/ViewModels/TasksViewModel.cs
@@ -14,13 +14,19 @@ public partial class TasksViewModel : ObservableRecipient
 
     public TasksViewModel()
     {
-        //TODO: move this to a generate-on-demand method instead of always creating and holding in memory when not needed?
+        //TODO: move this to a generate-on-demand method instead of always creating and holding in memory when not needed?  Sort of irrelevant,
+        //as it will be deleted or disabled eventually.
         Tasks.Clear();
         var sampleData = TaskEntity.GenerateSampleData();
         foreach (var row in sampleData)
         {
-            Tasks.Add(row);
+            Tasks.Add(row); //WOW: accidentally tried to use "Tasks.Append(row);" and it does not change Tasks, merely returns a modified copy of the list
         }
+    }
+
+    public string TaskCount()
+    {
+        return Tasks.Count() + " Tasks";
     }
 
 }
